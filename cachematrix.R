5b66a7e656a7f0972dfd51b4a611c24e48ae8b52
Claire Schulkey <claire.schulkey@gmail.com>
2025-03-24 15:59:55 -0400

    Update cachematrix.R
    Updated to include function machinery

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function creates a list of functions for use in lexical scoping; it creates the means to cache a matrix inverse for the following function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+  ##defining the list of functions
+  set <- function(y) {
+    
+    x <<- y
+    
+    m <<- NULL
+    
+  }
+  
+  get <- function() x
+  
+  setsolve <- function(solve) m <<- mean
+  
+  getsolve <- function() m
+
+##Returning the list of functions
+  
+  list(set = set, get = get,
+       
+       setsolve = setsolve,
+       
+       getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## Function which returns a matrix inverse by first checking if it has been calculated previously, and if not, calculating and returning the matrix inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+  ##checking whether inverse has been previously calculated and if so, returns that cached value
+  m <- x$getsolve()
+  
+  if(!is.null(m)) {
+    
+    message("getting cached data")
+    
+    return(m)
+    
+  }
+
+        ##if inverse has not been calculated, calculates inverse
+  
+  data <- x$get()
+  
+  m <- solve(data, ...)
+  
+  x$setsolve(m)
+  
+  m
+  
+
 }
